64c1db564b265b78648a98920d064bd85f649515
Ilham Yusuf Zulkarnain <7928466+ilhmyz@users.noreply.github.com>
2024-08-27 16:55:04 +0700

    [DEVOPS-3123]chore: Add tag to stack (#3)

--- bin/image-optimization.ts
+++ bin/image-optimization.ts
@@ -3,9 +3,12 @@ import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
 import { ImageOptimizationStack } from '../lib/image-optimization-stack';
 
-
 const app = new cdk.App();
-new ImageOptimizationStack(app, 'ImgTransformationStack', {
 
+const environment = app.node.tryGetContext('ENV');
+
+const stack = new ImageOptimizationStack(app, 'ImgTransformationStack', {
 });
 
+cdk.Tags.of(stack).add('Project', 'Popbela');
+cdk.Tags.of(stack).add('Environment', environment);
